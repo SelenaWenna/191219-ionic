01e7d0ab419c181293eae3e6e67866e9de1699d4
Selena Wenna <selenawenna@mail.ru>
2020-01-10 15:59:22 +0300

    Fix property subscribe

--- src/app/pages/hero-detail/hero-detail.component.ts
+++ src/app/pages/hero-detail/hero-detail.component.ts
@@ -50,8 +50,8 @@ export class HeroDetailComponent implements OnInit, OnDestroy {
      */
     getHero() {
         const {id} = this.route.snapshot.params;
-        return this.store.dispatch(new GetHero(id));
         this.hero$ = this.store.select(HeroesState.hero(id));
+        return this.store.dispatch(new GetHero(id));
     }
 
     /**
